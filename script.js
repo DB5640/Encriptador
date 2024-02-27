78d3952e3a0560fba0413d52f091debe1b2c3a81
DavidB <david.b5640@gmail.com>
2024-02-27 16:26:01 -0500

    Ahora encriptar se puede llamar desde el boton

--- script.js
+++ script.js
@@ -11,31 +11,29 @@ const matriz_code = [
     ["o", "ober"],
     ["u", "ufat"]
 ];
-var botonEncriptar = document.querySelector("#btnEncriptar");
-//TODO: agregar el eventListener para el boton 
-/* // Agregar un event listener para el evento de clic
-botonEncriptar.addEventListener("click", function() {
-    console.log("Has pulsado el boton");
-    let resultado = "";
-        console.log("Has pulsado la tecla Enter");
-        let texto = campoTexto.value;
-        for (let i = 0; i < texto.length; i++) {
-            if (/[aeiou]/gi.test(texto[i])) {
-                let encontrado = false;
-                for (let j = 0; !encontrado && j < matriz_code.length; j++) {
-                    if (matriz_code[j][0] === texto[i]) {
-                        resultado += matriz_code[j][1];
-                        encontrado = true;
+
+    const encriptar = () => {
+        let resultado = "";
+            console.log("Entre a la funcion encriptar");
+            let texto = campoTexto.value;
+            for (let i = 0; i < texto.length; i++) {
+                if (/[aeiou]/gi.test(texto[i])) {
+                    let encontrado = false;
+                    for (let j = 0; !encontrado && j < matriz_code.length; j++) {
+                        if (matriz_code[j][0] === texto[i]) {
+                            resultado += matriz_code[j][1];
+                            encontrado = true;
+                        }
                     }
+    
+                } else {
+                    resultado += texto[i];
                 }
-
-            } else {
-                resultado += texto[i];
             }
-        }
-        mensaje.innerHTML = resultado;
+            mensaje.innerHTML = resultado;
+    }
 
-}); */
+
 
 
 document.addEventListener("keydown", function (event) {

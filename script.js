6eaeebebae401fc6ada9fc418c5795654f5c6b74
DavidB <david.b5640@gmail.com>
2024-03-04 14:49:35 -0500

    Se agrega ValidateText que valida que no contenga números ni caracteres especiales

--- script.js
+++ script.js
@@ -22,21 +22,30 @@ const copiar = () => {
 const desencriptar = () => {
     let texto = campoTexto.value.toLowerCase();
             console.log("Texto en minusculas: ", texto);
-            
+    if (validateText(texto)) {
+        alert("El texto no puede contener números ni carácteres especiales");
+        return;
+    } else {
+        console.log("Entro al else de validateText")
             for (let i = 0; i < matriz_code.length; i++) {
                 texto = texto.replaceAll(matriz_code[i][1], matriz_code[i][0]);
             }
             
             mensaje.innerHTML = texto;
             campoTexto.value = "";
-            botonCopiar.innerHTML = "Copiar"
+        botonCopiar.innerHTML = "Copiar"
+    }
 }
 
 
     const encriptar = () => {
             let texto = campoTexto.value.toLowerCase();
             console.log("Texto en minusculas: ", texto);
-            
+        if (validateText(texto)) {
+            alert("El texto no puede contener números ni carácteres especiales");
+            return;
+        } else {
+            console.log("Entro al else de validateText")
             for (let i = 0; i < matriz_code.length; i++) {
            texto = texto.replaceAll(matriz_code[i][0], matriz_code[i][1]);
             }
@@ -44,7 +53,14 @@ const desencriptar = () => {
             mensaje.innerHTML = texto;
             campoTexto.value = "";
             botonCopiar.innerHTML = "Copiar"
+        }
 
+    }
+
+function validateText(texto) {
+    var regex = /^[1-9A-ZáéíóúÁÉÍÓÚñÑ]+$/;
+    console.log(`regex.test(texto) = ${regex.test(texto)}`);
+    return regex.test(texto);
     }
 
 
